f1c6aadfa5e02643374d2085783e5794538c6d90
Aron Ujvary <aronujvary@gmail.com>
2015-12-30 15:08:18 +0100

    A: add particle engine, basic impl and test code for rendering them

--- src/render/damage_particles.cpp
+++ src/render/damage_particles.cpp
@@ -3,6 +3,7 @@
 #include "damage_particles.h"
 #include "renderable_repo.h"
 #include "particle.h"
+#include "particle_engine.h"
 
 using core::DamageTakenEvent;
 
@@ -42,5 +43,6 @@ void DamageDecals::OnDamageTaken( DamageTakenEvent const& Evt )
     Part.mCenter = Evt.Pos + glm::vec2( ( rand() % 10 - 5.f ) / 200.f, ( rand() % 10 - 5.f ) / 200.f );
     Part.mId = mValidIds[rand() % mValidIds.size()];
     mDecalEngine.Add( Part, DecalEngine::GroundParticle );
+    render::ParticleEngine::Get().AddParticle( 0, Evt.Pos );
 }
 
